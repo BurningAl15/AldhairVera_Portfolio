9a1b24786fe5e1782bf48be20b2a0e3ea7868327
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-03-02 18:27:53 -0500

    Updates

---
+++ precache-manifest.655226d41d4f13184984e36e07d75414.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e8ed3ecaf7c412a55e8d1e4b9dafb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f72c7c4badfb1def15",
+    "url": "/static/css/main.753a2199.chunk.css"
+  },
+  {
+    "revision": "252d6b40374d8de48b0b",
+    "url": "/static/js/2.282ebaea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.282ebaea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80f72c7c4badfb1def15",
+    "url": "/static/js/main.bde94533.chunk.js"
+  },
+  {
+    "revision": "57e9cca9079c350ea6aa",
+    "url": "/static/js/runtime-main.f3f1aaf5.js"
+  },
+  {
+    "revision": "2641ead042a79e64fdc995bdde606948",
+    "url": "/static/media/CV.2641ead0.svg"
+  },
+  {
+    "revision": "81463cbb0d3f006e8d85877e3c90ba31",
+    "url": "/static/media/Facebook.81463cbb.svg"
+  },
+  {
+    "revision": "c9de0890dbca1837d3a6438c1a76f28d",
+    "url": "/static/media/Github.c9de0890.svg"
+  },
+  {
+    "revision": "d75b243c31b919c18d9e5eab56a42ee6",
+    "url": "/static/media/Instagram.d75b243c.svg"
+  },
+  {
+    "revision": "36d2bd3645b9fd1dfb4fab80d1a00d83",
+    "url": "/static/media/LinkedIn.36d2bd36.svg"
+  },
+  {
+    "revision": "320d92f548eadd806f877b38a37fc23c",
+    "url": "/static/media/NavIcon.320d92f5.svg"
+  },
+  {
+    "revision": "f9e74316985e34b93cc040816c860c4a",
+    "url": "/static/media/Twitter.f9e74316.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "65cb344c64e3d29ae35396789de64c2e",
+    "url": "/static/media/prstart.65cb344c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AldhairVera_Portfolio/precache-manifest.b11e68d36c5984fcd58c4a6cd93b8eff.js"
+  "/precache-manifest.655226d41d4f13184984e36e07d75414.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AldhairVera_Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
