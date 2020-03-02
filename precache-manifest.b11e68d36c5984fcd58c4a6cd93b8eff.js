6d93756b3d69c290eafd5dde0ee3a4534f5ccd32
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-03-02 16:06:04 -0500

    Updates

---
+++ precache-manifest.b11e68d36c5984fcd58c4a6cd93b8eff.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0b653e8b06f376b3bdd5a08e15588d",
+    "url": "/AldhairVera_Portfolio/index.html"
+  },
+  {
+    "revision": "e598a7de4bfcb83c85d5",
+    "url": "/AldhairVera_Portfolio/static/css/main.b3e36d06.chunk.css"
+  },
+  {
+    "revision": "252d6b40374d8de48b0b",
+    "url": "/AldhairVera_Portfolio/static/js/2.282ebaea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AldhairVera_Portfolio/static/js/2.282ebaea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e598a7de4bfcb83c85d5",
+    "url": "/AldhairVera_Portfolio/static/js/main.92a0d523.chunk.js"
+  },
+  {
+    "revision": "9eb84f99e963ec773b2b",
+    "url": "/AldhairVera_Portfolio/static/js/runtime-main.55615eae.js"
+  },
+  {
+    "revision": "2641ead042a79e64fdc995bdde606948",
+    "url": "/AldhairVera_Portfolio/static/media/CV.2641ead0.svg"
+  },
+  {
+    "revision": "81463cbb0d3f006e8d85877e3c90ba31",
+    "url": "/AldhairVera_Portfolio/static/media/Facebook.81463cbb.svg"
+  },
+  {
+    "revision": "c9de0890dbca1837d3a6438c1a76f28d",
+    "url": "/AldhairVera_Portfolio/static/media/Github.c9de0890.svg"
+  },
+  {
+    "revision": "d75b243c31b919c18d9e5eab56a42ee6",
+    "url": "/AldhairVera_Portfolio/static/media/Instagram.d75b243c.svg"
+  },
+  {
+    "revision": "36d2bd3645b9fd1dfb4fab80d1a00d83",
+    "url": "/AldhairVera_Portfolio/static/media/LinkedIn.36d2bd36.svg"
+  },
+  {
+    "revision": "320d92f548eadd806f877b38a37fc23c",
+    "url": "/AldhairVera_Portfolio/static/media/NavIcon.320d92f5.svg"
+  },
+  {
+    "revision": "f9e74316985e34b93cc040816c860c4a",
+    "url": "/AldhairVera_Portfolio/static/media/Twitter.f9e74316.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/AldhairVera_Portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "65cb344c64e3d29ae35396789de64c2e",
+    "url": "/AldhairVera_Portfolio/static/media/prstart.65cb344c.ttf"
+  }
+]);
--- src/Page/Games.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class Games extends React.Component {
-  render() {
-    return <div>GAMES!</div>;
-  }
-}
-export default Games;
